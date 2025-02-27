1caa721a319a92f51316be66ee9c1a81d1d8e4de
bogdanbasanko <bogdanbasanko2007@gmail.com>
2025-02-27 15:31:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-#import snastik as avgGrade
-#opilane = {
-#    "nimi": "Thomas",
-#    "vanus": 18,
-#    "klass": "12A"
-#}
-#print(opilane["nimi"])
-#print(opilane.get("vanus"))
-
-#opilane["adress"] = "Tallinn"
-#opilane.pop("nimi")
-#del opilane["klass"]
-
-#print(opilane.values())
-#print(opilane.keys())
-
-#for elem in opilane.keys():
-  #  print(elem)
-   
-#for key, value in opilane.items():
-  #  print("see on võtti", key, "see on väärtus", value)
-
-#grades = {
-  #  "Mari": [4,5,3,4]
-   # "Jaan": [2,2,3,2]
-#}
-#avgGrade.arvutaKeskmineHinne(grades)
-#}
-#result = avgGrade.arvutaKeskmineHinne(grades)
-#print(result)
-
-
-from snastik import letter_frequency
-
-word = input("Введите слово: ")
-result = letter_frequency(word)
-print("Частота букв в слове:", result)
+def guestBook():
+    name = input("sinu nimi: ")
+    comment = input("sinu sõnum: ")
+    text = f"nimi: "{name}, "Sõnum: " {comment}
+    file = open("guestBook.txt", "a")
+    fil.write(text)
+    file.close()
+    print("sõnum oli salvestatu edukalt")
